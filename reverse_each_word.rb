119bc7241164635537a1fa0edb1d60e49ce96f0b
Tracy Ngo <tngo1206@gmail.com>
2019-02-09 22:46:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 string = "Hello there, and how are you?"
 
 def reverse_each_word(string)
-string.split(" ")
+string_array =string.split(" ")
 reversed_array= string.collect { |n| n.reverse }
 reversed_array.join(" ")
 end
